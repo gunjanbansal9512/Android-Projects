14f70876308090675d9b773a569acff780aec82f
gunjanbansal9512 <gunjanbansalcitech@gmail.com>
2019-10-23 20:00:52 +0530

    Database App

--- DatabaseApp/app/src/main/java/com/example/databaseapp/DHelper.java
+++ DatabaseApp/app/src/main/java/com/example/databaseapp/DHelper.java
@@ -1,30 +1,71 @@
 package com.example.databaseapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 public class DHelper extends SQLiteOpenHelper {
     public static String DB_Name="student_database";
     public static String Table_Name="student";
-    public static final String col1="usn";
+    public static final String col1="id";
     public static final String col2="name";
     public static final String col3="marks";
     private Context context;
-    public static final String table_create ="CREATE TABLE student_id (INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT, marks INT(2))";
+    public static final String table_create ="CREATE TABLE student ( id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT, marks INT(2))";
     public DHelper(@Nullable Context context) {
         super(context, DB_Name, null, 3);
         this.context=context;
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        Toast.makeText(context,"In onCreate()",Toast.LENGTH_LONG).show();
+    try
+    {
+        db.execSQL(table_create);
+    }
+    catch(Exception e)
+    {
+        Toast.makeText(context,e.getMessage(),Toast.LENGTH_LONG).show();
+    }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        Toast.makeText(context,"In onUpgarde()",Toast.LENGTH_LONG).show();
+    sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+Table_Name);
+    onCreate(sqLiteDatabase);
+    }
+    public boolean insertData(String name,String marks)
+    {
+        SQLiteDatabase db= this.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+        cv.put(col2,name);
+        cv.put(col3,marks);
+        long result=db.insert(Table_Name,null,cv);
+        if(result==-1)
+            return false;
+        else
+            return true;
+    }
+    public boolean updateData(String id, String name,String marks)
+    {
+       SQLiteDatabase db = getWritableDatabase();
+       ContentValues cv = new ContentValues();
+       cv.put(col1,id);
+       cv.put(col2,name);
+       cv.put(col3,marks);
+       db.update(Table_Name,cv,"id=?",new String[]{id});
+        return true;
+    }
+    public Cursor getData()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cur = db.rawQuery("SELECT * FROM "+Table_Name,null);
+        return cur;
     }
 }
--- DatabaseApp/app/src/main/java/com/example/databaseapp/MainActivity.java
+++ DatabaseApp/app/src/main/java/com/example/databaseapp/MainActivity.java
@@ -1,13 +1,78 @@
 package com.example.databaseapp;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button insert,select,update;
+EditText id,name,marks;
+DHelper dHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    insert=(Button)findViewById(R.id.insert);
+    select=(Button)findViewById(R.id.display);
+    update=(Button)findViewById(R.id.delete);
+    id=(EditText)findViewById(R.id.id);
+    name=(EditText)findViewById(R.id.name);
+    marks=(EditText)findViewById(R.id.marks);
+    dHelper = new DHelper(this);
+    insert.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+boolean result=dHelper.insertData(name.getText().toString(),marks.getText().toString());
+if(result==false)
+{
+    Toast.makeText(getApplicationContext(),"Error in insertion",Toast.LENGTH_LONG).show();
+}
+else
+    Toast.makeText(getApplicationContext(),"Data is inserted",Toast.LENGTH_LONG).show();
+
+        }
+    });
+    select.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Cursor cur=dHelper.getData();
+            if(cur.getCount()==0)
+            {
+                Toast.makeText(getApplicationContext(),"No data",Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                StringBuffer buffer = new StringBuffer();
+                while(cur.moveToNext())
+                {
+                    buffer.append("id "+cur.getString(0)+"\n");
+                    buffer.append("name "+cur.getString(1)+"\n");
+                    buffer.append("marks "+cur.getString(2)+"\n");
+                }
+                showMessage("Data",buffer.toString());
+            }
+        }
+    });
+    update.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            boolean result = dHelper.updateData(id.getText().toString(),name.getText().toString(),marks.getText().toString());
+            if(result==true)
+                Toast.makeText(MainActivity.this, "Data updated sucessfully", Toast.LENGTH_SHORT).show();
+        }
+    });
+    }
+    public  void showMessage(String title,String data)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(data);
+        builder.show();
     }
 }
