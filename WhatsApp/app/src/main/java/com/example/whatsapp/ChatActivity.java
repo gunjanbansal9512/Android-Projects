d55a8794bcb71eaaa4e24b935cfef6afc3b7d921
gunjanbansal9512 <gunjanbansalcitech@gmail.com>
2019-11-11 22:25:46 +0530

    "WhatsApp"

--- WhatsApp/app/src/main/java/com/example/whatsapp/ChatActivity.java
+++ WhatsApp/app/src/main/java/com/example/whatsapp/ChatActivity.java
@@ -1,17 +1,40 @@
 package com.example.whatsapp;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
 private String messageReceiverId,messageReceiverName;
+private TextView userName,userLastSeen;
+private CircleImageView userImage;
+private Toolbar chatToolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        chatToolBar=(Toolbar)findViewById(R.id.chat_toolbar);
+        setSupportActionBar(chatToolBar);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater layoutInflater=(LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View actionBarView=layoutInflater.inflate(R.layout.custom_chat_bar,null);
+        actionBar.setCustomView(actionBarView);
+        userName=(TextView)findViewById(R.id.custom_profile_name);
+        userLastSeen=(TextView)findViewById(R.id.custom_user_last_seen);
+        userImage=(CircleImageView)findViewById(R.id.custom_profile_image);
         messageReceiverId=getIntent().getExtras().get("visit_user_id").toString();
         messageReceiverName=getIntent().getExtras().get("visit_user_name").toString();
-        Toast.makeText(this, ""+messageReceiverId+"\n"+messageReceiverName, Toast.LENGTH_SHORT).show();
+        userName.setText(messageReceiverName);
     }
 }
